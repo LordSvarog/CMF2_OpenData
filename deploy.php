22e7a8628e66d2dd8ad0aa6995f93e34edd692a3
Alexander <yii2.developer@gmail.com>
2019-08-16 11:04:23 +0500

    #97274 - Подключить: deployer (CMF2)

---
+++ deploy.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/rsync.php';
+require 'recipe/cmf2.php';
+
+// Configuration
+
+if (!has('ssh_directory')) {
+    set('ssh_directory', getenv('SSH_DIRECTORY'));
+}
+if (!has('ssh_host')) {
+    set('ssh_host', getenv('SSH_HOST'));
+}
+if (!has('ssh_port')) {
+    set('ssh_port', getenv('SSH_PORT'));
+}
+if (!has('ssh_username')) {
+    set('ssh_username', getenv('SSH_USERNAME'));
+}
+
+set('rsync', [
+    'exclude' => [],
+    'exclude-file' => 'exclude.txt',
+    'include' => [],
+    'include-file' => 'include.txt',
+    'filter' => [],
+    'filter-file' => false,
+    'filter-perdir' => false,
+    'flags' => 'rz',
+    'options' => ['links', 'delete'],
+    'timeout' => 3600,
+]);
+
+// Hosts
+
+host(get('ssh_host'))
+    ->user(get('ssh_username'))
+    ->forwardAgent(false)
+    ->multiplexing(true)
+    ->sshFlags([
+        '-p' => get('ssh_port', 22),
+    ])
+    ->sshOptions([
+        'StrictHostKeyChecking' => 'no',
+    ])
+    ->set('deploy_path', '{{ssh_directory}}')
+    ->set('rsync_src', '.')
+    ->set('rsync_dest', '{{release_path}}');
+
+task('deploy:develop', function () {
+    invoke('deploy:prepare');
+    invoke('deploy:lock');
+    invoke('deploy:release');
+    invoke('rsync');
+    invoke('deploy:symlink');
+    invoke('deploy:unlock');
+    invoke('success');
+})->desc(
+    'Deploy your project'
+);
+
+task('deploy:production', function () {
+    invoke('deploy:prepare');
+    invoke('deploy:lock');
+    invoke('deploy:release');
+    invoke('rsync');
+    invoke('deploy:symlink');
+    invoke('deploy:unlock');
+    invoke('success');
+})->desc(
+    'Deploy your project'
+);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
